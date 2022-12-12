b5bdf0ba459141275b711afe529dce3d169a01d7
22MH1A05I3 <120080322+22MH1A05I3@users.noreply.github.com>
2022-12-12 21:45:47 +0530

    #include<stdio.h> int main() {     int n,i=1,sum=0;     scanf("%d",&n);     while(i<n)     {         if(n%i==0)         {             sum=sum+i;         }
            i++;
        }
        
        printf("%d",sum);
    }

--- _sum_of_factors.c
+++ _sum_of_factors.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int n,i=1,sum=0;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    while(i<n)
     {
         if(n%i==0)
         {
             sum=sum+i;
         }
+        
+        i++;
     }
+    
     printf("%d",sum);
 }
