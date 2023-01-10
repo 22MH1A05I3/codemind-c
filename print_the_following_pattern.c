4c08468822e5fca385d2cec859a79af3f77275dc
NIHHAL SHANKAR KARANAM <120080322+22MH1A05I3@users.noreply.github.com>
2023-01-10 11:05:25 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",i+64);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,9 +3,9 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=i;j>=1;j--)
+        for(j=1;j<=n;j++)
         {
             printf("%c ",i+64);
         }
