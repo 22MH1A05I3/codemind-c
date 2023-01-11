febb5fdf0e8ed230d2375b0ec62482355d3838c4
NIHHAL SHANKAR KARANAM <120080322+22MH1A05I3@users.noreply.github.com>
2023-01-11 10:46:03 +0530

    #include<stdio.h> int main() {     int a,x,y,z;     scanf("%d%d%d%d",&a,&x,&y,&z);     if(x+y==a||y+z==a||z+x==a||x==a||y==a||z==a)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Measuring_the_Object.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,x,y,z;
+    scanf("%d%d%d%d",&a,&x,&y,&z);
+    if(x+y==a||y+z==a||z+x==a||x==a||y==a||z==a)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
